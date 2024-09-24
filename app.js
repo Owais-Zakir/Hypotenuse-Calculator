416caedcdd53aee98a8db337c1eb4f77fe6ae407
Owais-Zakir <owaiszakir99@gmail.com>
2024-09-24 23:14:34 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let a = +prompt("Enter The Value Of A");
+let b = +prompt("Enter The Value Of B");
+let c = a**2+b**2;
+let finalValue = Math.sqrt(c);
+document.getElementById("head2").innerHTML = finalValue;
